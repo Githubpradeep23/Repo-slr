e9bfc1311b735b8619b6ec8a48005ea169ff9c30
Githubpradeep23 <123489512+Githubpradeep23@users.noreply.github.com>
2023-10-13 17:31:53 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "",
-  output: "standalone",
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    domains: ["localhost", "localhost:3000", "https://githubpradeep23.github.io/web-slr/"],
-  },
+module.exports = {
+  output: "export",
+  // Other configurations
 };
-
-module.exports = nextConfig
